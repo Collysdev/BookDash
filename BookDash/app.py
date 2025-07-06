311c502690afa0ca4ffb0d6ad1e73fbb219d8b41
Nicolly Santos <152344710+Collysdev@users.noreply.github.com>
2025-07-05 21:17:18 -0300

    Update app.py

--- BookDash/app.py
+++ BookDash/app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-df_reviews = pd.read_csv("BookDash/datasets/customer_reviews.csv")
+df_reviews = pd.read_csv("datasets/customer_reviews.csv")
 df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
 price_max = df_top100_books["book price"].max()
